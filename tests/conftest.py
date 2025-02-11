5b5cc398b57bbfd47ea1939488dc993af9e9d83f
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-11 13:31:43 +0300

    Изменена фикстура для запуска браузера

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,51 @@
+import os
 import pytest
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
+from dotenv import load_dotenv
+import allure
+
+DEFAULT_BROWSER_VERSION = '126.0'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='120.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(autouse=True, scope='function')
-def browser_management():
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "120.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
+def browser_management(request):
+    with allure.step("Параметры браузера"):
+        browser_version = request.config.getoption('--browser_version')
+
+        browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": browser_version,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
         }
-    }
 
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    selenoid_host = os.getenv('SELENOID_HOST')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f'https://{login}:{password}@{selenoid_host}/wd/hub',
         options=options)
 
     browser.config.driver = driver

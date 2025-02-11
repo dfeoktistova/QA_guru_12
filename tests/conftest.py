32a783dd820e4fe9a2e1d2f3aad868e11502c975
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-11 13:22:47 +0300

    add old files

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils import attach
+
+
+@pytest.fixture(autouse=True, scope='function')
+def browser_management():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "120.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+
+    browser.config.timeout = 2.0
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
---
+++ tests/test_fill_form.py
@@ -0,0 +1,69 @@
+from model.registration_page import RegistrationPage
+import allure
+
+first_name = 'Santa'
+last_name = 'Claus'
+email = f'{first_name}@mail.com'
+user_number = '1234567890'
+birth_day = '11'
+birth_month = 'April'
+birth_year = '2007'
+gender = "Male"
+subjects = 'Maths'
+hobby = 'Reading'
+picture = 'pic.webp'
+address = 'Zamshina street, 11/5'
+state = 'NCR'
+city = 'Noida'
+
+
+def test_fill_form():
+    registration_page = RegistrationPage()
+
+    with allure.step("Открыть страницу регистрации"):
+        registration_page.open()
+
+    with allure.step("Заполнить поле 'first_name'"):
+        registration_page.fill_first_name(first_name)
+
+    with allure.step("Заполнить поле 'last_name'"):
+        registration_page.fill_last_name(last_name)
+
+    with allure.step("Заполнить поле 'email'"):
+        registration_page.fill_email(email)
+
+    with allure.step("Заполнить поле 'gender'"):
+        registration_page.select_gender(gender)
+
+    with allure.step("Заполнить поле 'user_number'"):
+        registration_page.fill_user_number(user_number)
+
+    with allure.step("Заполнить поля 'birth_month' и 'birth_year'"):
+        registration_page.select_date_of_birth(birth_month, birth_year)
+
+    with allure.step("Заполнить поле 'subjects'"):
+        registration_page.fill_subjects(subjects)
+
+    with allure.step("Заполнить поле 'hobby'"):
+        registration_page.select_hobbies(hobby)
+
+    with allure.step("Заполнить поле 'picture'"):
+        registration_page.upload_picture(picture)
+
+    with allure.step("Заполнить поле 'address'"):
+        registration_page.fill_address(address)
+
+    with allure.step("Заполнить поле 'state'"):
+        registration_page.select_state(state)
+
+    with allure.step("Заполнить поле 'city'"):
+        registration_page.select_city(city)
+
+    with allure.step("Подтвердить регистрацию"):
+        registration_page.submit()
+
+    with allure.step("Сравнить результаты"):
+        registration_page.should_have_registered(first_name, last_name, email, gender, user_number, birth_day, birth_month,
+                                                 birth_year, subjects, picture, address, state, city)
+
+

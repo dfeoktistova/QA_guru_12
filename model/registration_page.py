92e2b2adbc2cccb082cce0279579705354938732
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-18 11:12:31 +0300

    Добавлено удаление баннеров

--- model/registration_page.py
+++ model/registration_page.py
@@ -21,10 +21,12 @@ class RegistrationPage:
         self.close_button = browser.element('#closeLargeModal')
 
     def open(self):
+        browser.open('https://demoqa.com/automation-practice-form')
+
         browser.driver.execute_script("$('#fixedban').remove()")
         browser.driver.execute_script("$('footer').remove()")
 
-        browser.open('https://demoqa.com/automation-practice-form')
+        browser.driver.refresh()
 
     def fill_first_name(self, value):
         self.first_name.type(value)
